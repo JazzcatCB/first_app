e0ec0e5c10c3486c7663fb07d3853c995c6a3c86
Chris Barnhill <JazzcatCB@gmail.com>
2011-12-27 18:02:55 -0800

    Added configuration for exception handler to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 FirstApp::Application.initialize!
+
+# Include your application configuration below
+
+ExceptionNotifier.exception_recipients = %w(JazzcatCB@gmail.com)
+
+ExceptionNotifier.sender_address = %("Application Error" <JazzcatCB@gmail.com>)
+
+# defaults to "[ERROR] "
+
+ExceptionNotifier.email_prefix = "[FIRST_APP] "
+
